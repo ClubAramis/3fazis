c47812fe21548ac39e98338b84feb1e9b5fadd1c
pleve <plevente2002@gmail.com>
2023-05-17 16:40:48 +0200

    updating git [RF-00000]

---
+++ Hotcakes_orders/Hotcakes_orders/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Hotcakes_orders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.ordersDataGridView = new System.Windows.Forms.DataGridView();
+            this.createOrderButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ordersDataGridView
+            // 
+            this.ordersDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ordersDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.ordersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ordersDataGridView.DefaultCellStyle = dataGridViewCellStyle11;
+            this.ordersDataGridView.Location = new System.Drawing.Point(181, 12);
+            this.ordersDataGridView.Name = "ordersDataGridView";
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ordersDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
+            this.ordersDataGridView.RowHeadersWidth = 51;
+            this.ordersDataGridView.RowTemplate.Height = 24;
+            this.ordersDataGridView.Size = new System.Drawing.Size(586, 276);
+            this.ordersDataGridView.TabIndex = 0;
+            // 
+            // createOrderButton
+            // 
+            this.createOrderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.createOrderButton.Location = new System.Drawing.Point(12, 304);
+            this.createOrderButton.Name = "createOrderButton";
+            this.createOrderButton.Size = new System.Drawing.Size(173, 42);
+            this.createOrderButton.TabIndex = 1;
+            this.createOrderButton.Text = "Módosítás";
+            this.createOrderButton.UseVisualStyleBackColor = true;
+            this.createOrderButton.Click += new System.EventHandler(this.createOrderButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 196);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Bezárás";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 314);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ár:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.createOrderButton);
+            this.Controls.Add(this.ordersDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ordersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ordersDataGridView;
+        private System.Windows.Forms.Button createOrderButton;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hotcakes_orders/Hotcakes_orders/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1.Client;
+using Hotcakes.CommerceDTO.v1.Contacts;
+using Hotcakes.CommerceDTO.v1.Orders;
+using Newtonsoft.Json;
+
+namespace Hotcakes_orders
+{
+    public partial class Form1 : Form
+    {
+        public IDictionary<string, string> kategoriak = new Dictionary<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+            GetCategory();
+            GetProduct();
+        }
+
+        private void createOrderButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://20.234.113.211:8091/";
+            string key = "1-36f61538-6409-4681-bce7-abd31e56fe80";
+
+            Api proxy = new Api(url, key);
+
+            // create a new order object
+            var order = new OrderDTO();
+
+            // add billing information
+            order.BillingAddress = new AddressDTO
+            {
+                AddressType = AddressTypesDTO.Billing,
+                City = "West Palm Beach",
+                CountryBvin = "BF7389A2-9B21-4D33-B276-23C9C18EA0C0",
+                FirstName = "John",
+                LastName = "Dough",
+                Line1 = "319 N. Clematis Street",
+                Line2 = "Suite 600",
+                Phone = "561-228-5319",
+                PostalCode = "33401",
+                RegionBvin = "7EBE4F07-A844-47B8-BDA8-863DDDF5C778"
+            };
+
+            // add at least one line item
+            order.Items = new List<LineItemDTO>();
+            order.Items.Add(new LineItemDTO
+            {
+                ProductId = "dfcae0ee-8bcf-4321-8b31-7883b5434285",
+                Quantity = 1
+            });
+
+            // add the shipping address
+            order.ShippingAddress = new AddressDTO();
+            order.ShippingAddress = order.BillingAddress;
+            order.ShippingAddress.AddressType = AddressTypesDTO.Shipping;
+
+            // specify who is creating the order
+            order.UserEmail = "info@hotcakescommerce.com";
+            order.UserID = "1";
+
+            // call the API to create the order
+            ApiResponse<OrderDTO> response = proxy.OrdersCreate(order);
+
+            GetCategory();
+        }
+
+        private void deleteOrderButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://20.234.113.211:8091/";
+            string key = "1-36f61538-6409-4681-bce7-abd31e56fe80";
+
+            Api proxy = new Api(url, key);
+
+            // specify the order to delete
+            int rowIndex = ordersDataGridView.CurrentCell.RowIndex;
+            int columnIndex = ordersDataGridView.CurrentCell.ColumnIndex;
+
+            var orderId = ordersDataGridView[columnIndex, rowIndex].Value.ToString();
+
+            // call the API to delete the order
+            ApiResponse<bool> response = proxy.OrdersDelete(orderId);
+
+            GetCategory();
+        }
+
+        public void GetCategory()
+        {
+            string url = "http://20.234.113.211:8091/";
+            string key = "1-36f61538-6409-4681-bce7-abd31e56fe80";
+
+            Api proxy = new Api(url, key);
+
+            // call the API to find all orders in the store
+            ApiResponse<List<CategorySnapshotDTO>> response = proxy.CategoriesFindAll();
+            string json = JsonConvert.SerializeObject(response);
+
+            ApiResponse<List<CategorySnapshotDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<CategorySnapshotDTO>>>(json);
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Bvin", typeof(string));
+            dt.Columns.Add("Name", typeof(string));
+
+            foreach (CategorySnapshotDTO item in deserializedResponse.Content)
+            {
+                kategoriak.Add(item.Name, item.Bvin);
+                dt.Rows.Add(item.Bvin, item.Name);
+            }
+
+            listBox1.DisplayMember = "Name";
+            listBox1.DataSource = dt;
+        }
+
+        public void GetProduct()
+        {
+            string url = "http://20.234.113.211:8091/";
+            string key = "1-36f61538-6409-4681-bce7-abd31e56fe80";
+        
+            Api proxy = new Api(url, key);
+
+            CategoryDTO ujkat = new CategoryDTO();
+
+            //CategoryDTO kat = listBox1.SelectedItem as CategoryDTO;
+
+            ujkat.Name = listBox1.GetItemText(listBox1.SelectedItem);
+
+            //Console.WriteLine(ujkat.Bvin);
+        
+            // specify a category to look for products in
+            var katID = kategoriak[ujkat.Name];
+        
+            // specify the page number to retrieve
+            var page = 1;
+        
+            // specify the number of results per page
+            var pageSize = int.MaxValue; // if you want all products, then use int.MaxValue here
+        
+            
+        
+            // call the API to find all products in the store matching the category & page criteria
+            ApiResponse<PageOfProducts> response = proxy.ProductsFindForCategory(katID, page, pageSize);
+            string json = JsonConvert.SerializeObject(response);
+            
+            
+            ordersDataGridView.DataSource = response.Content.Products.ToList();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetProduct();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //
+        //}
+    }
+}
